40fb3045cdc518ecb628c90818bed537a5af291d
baptiste dubot <baptiste.dubot56@gmail.com>
2024-10-11 17:48:56 +0200

    Modification of the connexion and inscription forme to add style and methode, remove of component inscription, connexion, choix

--- src/app/sessions/sessions.component.ts
+++ src/app/sessions/sessions.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { ChoseComponent } from './component/chose/chose.component';
-import { ConnexionComponent } from './component/connexion/connexion.component';
-import { InscriptionComponent } from './component/inscription/inscription.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sessions',
   standalone: true,
-  imports: [ChoseComponent, ConnexionComponent, InscriptionComponent],
+  imports: [],
   templateUrl: './sessions.component.html',
   styleUrl: './sessions.component.scss'
 })
 export class SessionsComponent {
+  
+  public inscriptionForm: FormGroup;
 
+  public inscription_steps: number = 1;
+  public connexion_steps: number = 0;
+
+  public progress: number = 0;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.inscriptionForm = this.fb.group({
+      phone_number: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      sexe: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 }
